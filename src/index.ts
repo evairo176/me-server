b3d46a0f2c54b752170083ac79e691a60d4d80d3
Dicki Prasetya <dickiprasetya176@gmail.com>
2023-10-23 14:03:39 +0700

    feat: update

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
-// show image
+// show image static
 app.use(express.static("public"));
 
 // blogs routes
